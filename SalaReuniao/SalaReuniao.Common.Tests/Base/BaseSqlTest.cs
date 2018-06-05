7b95a3a5bbdc8ab8f9961924f41a6c98f1b2dc92
lipefesliz <felipeliz1986@hotmail.com>
2018-06-05 20:23:37 -0300

    criado nova tabela TBRooms e adicionado metodo de seeding em BaseSql

--- SalaReuniao/SalaReuniao.Common.Tests/Base/BaseSqlTest.cs
+++ SalaReuniao/SalaReuniao.Common.Tests/Base/BaseSqlTest.cs
@@ -19,6 +19,19 @@ namespace SalaReuniao.Common.Tests.Base
                  'Desenvolvedor',
                  1)";
 
+        /* TBRooms */
+        private const string RECREATE_ROOM_TABLE = @"DELETE FROM [dbo].[TBRooms]";
+
+        private const string RESET_ROOM_IDENTITY = @"DBCC CHECKIDENT('TBRooms', RESEED, 0)";
+
+        private const string INSERT_ROOM =
+            @"INSERT INTO TBROOMS
+                (NAME,
+                 CHAIRS)
+            VALUES
+                ('sala de treinamento',
+                 10)";
+
         /* TBSchedules */
         private const string RECREATE_SCHEDULE_TABLE = @"TRUNCATE TABLE [dbo].[TBSchedules]";
 
@@ -26,15 +39,13 @@ namespace SalaReuniao.Common.Tests.Base
             @"INSERT INTO TBSCHEDULES
                 (STARTING,
                  ENDING,
-                 ROOM,
-                 CHAIRS,
+                 ROOMID,
                  EMPLOYEEID,
                  ISAVAILABLE)
             VALUES
                 (GETDATE(),
                  GETDATE() + 0.002,
                  1,
-                 20,
                  1,
                  1)";
 
@@ -45,6 +56,10 @@ namespace SalaReuniao.Common.Tests.Base
             Db.Update(RECREATE_EMPLOYEE_TABLE);
             Db.Update(RESET_EMPLOYEE_IDENTITY);
             Db.Update(INSERT_EMPLOYEE);
+
+            Db.Update(RECREATE_ROOM_TABLE);
+            Db.Update(RESET_ROOM_IDENTITY);
+            Db.Update(INSERT_ROOM);
 
             Db.Update(INSERT_SCHEDULE);
         }
--- SalaReuniao/SalaReuniao.Database/dbo/Tables/TBSchedules.sql
+++ SalaReuniao/SalaReuniao.Database/dbo/Tables/TBSchedules.sql
@@ -3,9 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1), 
     [Starting] DATETIME NOT NULL, 
     [Ending] DATETIME NOT NULL, 
-    [Room] INT NOT NULL, 
-    [Chairs] INT NOT NULL, 
+    [RoomId] INT NOT NULL, 
 	[EmployeeId] INT NOT NULL, 
 	[IsAvailable] BIT NOT NULL 
+    CONSTRAINT [FK_Schedules_Room] FOREIGN KEY ([RoomId]) REFERENCES [TBRooms]([Id])
     CONSTRAINT [FK_Schedules_Employee] FOREIGN KEY ([EmployeeId]) REFERENCES [TBEmployees]([Id])
 )
