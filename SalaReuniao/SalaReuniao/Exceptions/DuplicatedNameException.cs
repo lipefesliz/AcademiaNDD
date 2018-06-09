6c0b2ac7d477527e39ca15143e51df7709ab5c57
lipefesliz <felipeliz1986@hotmail.com>
2018-06-08 23:46:27 -0300

    testes do servico

--- SalaReuniao/SalaReuniao/Exceptions/DuplicatedNameException.cs
+++ SalaReuniao/SalaReuniao/Exceptions/DuplicatedNameException.cs
@@ -1,6 +1,6 @@
 ﻿using SalaReuniao.Domain.Exceptions;
 
-namespace SalaReuniao.Features.Employees.Exceptions
+namespace SalaReuniao.Domain.Exceptions
 {
     public class DuplicatedNameException : BusinessException
     {
